308da8f88aea8d71759e1d0a3e3f563610830c7d
Dave Powell <evadllewop@gmail.com>
2020-10-22 13:07:53 -0500

    updated questions

--- Assets/script.js
+++ Assets/script.js
@@ -5,14 +5,23 @@ var qEl = document.getElementById("question");
 var timeEl = document.getElementById("timer");
 var counter = document.getElementById("counter");
 var timeGauge = document.getElementById("timeGauge");
+var scoreDiv = document.getElementById("scoreContainer");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
-// var answerStat = document.getElementById("answerStat");
+var answerStat = document.getElementById("answerStat");
+var highScoresEl = document.getElementById("highScores");
+
+var highScores = document.createElement('a');
+highScores.textContent = "View Highscores";
+highScoresEl.appendChild(highScores);
+highScores.href = "#";
+highScores.setAttribute("style", "margin-top:40px; font-size:16px; text-align:center;");
+
 
 
 var h1El = document.createElement("h1");
-h1El.textContent = "Welcome To My Quiz";
+h1El.textContent = "My Javascript Quiz";
 introEl.appendChild(h1El);
 h1El.setAttribute("style", "margin-bottom:20px; font-size:50px; text-align:center;");
 
@@ -33,44 +42,55 @@ qEl.setAttribute("style", "margin-bottom:20px; text-align:center;");
 
 var questions = [
     {
-        question: "What is my name?",
-        choiceA: "George",
-        choiceB: "Charlie",
-        choiceC: "Dave",
+        question: "Which of the following are capabilities of functions in JavaScript?",
+        choiceA: "Return a value",
+        choiceB: "Accept parameters and Return a value",
+        choiceC: "Accept parameters",
         correct: "C"
     }, {
-        question: "What is my boy cat's name?",
-        choiceA: "Abigail",
-        choiceB: "Dario",
-        choiceC: "Stanley",
+        question: "Which of the following is not a valid JavaScript variable name?",
+        choiceA: "2names",
+        choiceB: "_first_and_last_names",
+        choiceC: "FirstAndLast",
+        correct: "A"
+    }, {
+        question: "It is possible to nest functions in JavaScript",
+        choiceA: "True",
+        choiceB: "False",
+        choiceC: "Neither",
+        correct: "A"
+    }, {
+        question: "What is mean by “this” keyword in javascript?",
+        choiceA: "It referes previous object",
+        choiceB: "It refers current object",
+        choiceC: "It is variable which contains value",
         correct: "B"
     }, {
-        question: "What is my wife's name?",
-        choiceA: "Etholine",
-        choiceB: "Gertrudine",
-        choiceC: "LaDonna",
-        correct: "C"
-    }
+        question: "What are variables used for in JavaScript?",
+        choiceA: "Storing numbers, dates, or other values",
+        choiceB: "Varying randomly",
+        choiceC: "Causing high-school algebra flashbacks",
+        correct: "A"
+    },
 ];
 
 var lastQuestion = questions.length - 1;
 var runningQuestion = 0;
 var questionTime = 10;
 var count = 0;
-// const gaugeWidth = 150; // 150px
-// const gaugeUnit = gaugeWidth / questionTime;
-// let TIMER;
-let score = 0;
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+// var score = 0;
 
 
 buttonEl.addEventListener("click", startQuiz);
 
 function startQuiz() {
     introEl.style.display = "none";
+    setTime();
     renderQuestion();
     quiz.style.display = "block";
-    // renderProgress();
-    setTime();
 }
 
 function renderQuestion() {
@@ -88,13 +108,11 @@ function checkAnswer(answer) {
     console.log(answer);
     var score = 0;
     if (questions[runningQuestion].correct == answer) {
-        // answer is correct
+
         score++;
-        // change progress color to green
+        alert(score);
         answerIsCorrect();
     } else {
-        // answer is wrong
-        // change progress color to red
         answerIsWrong();
     }
     count = 0;
@@ -102,19 +120,16 @@ function checkAnswer(answer) {
         runningQuestion++;
         renderQuestion();
     } else {
-        // end the quiz and show the score
-        clearInterval(TIMER);
         scoreRender();
     }
 }
 
 function answerIsCorrect() {
-    // console.log("yep");
+
     var answerCorrect = document.createElement("p");
     answerCorrect.textContent = "Correct";
     answerStat.appendChild(answerCorrect);
-    answerCorrect.setAttribute("style", "color:green; font-size:20px; font-weight:bold; margin-top:20px; text-align:center;");
-
+    answerCorrect.setAttribute("style", "color:green; font-size:24px; margin-top:20px; text-align:center;");
     var answerTime = 1;
     var answerInterval = setInterval(function () {
         answerTime--;
@@ -123,21 +138,15 @@ function answerIsCorrect() {
             clearInterval(answerInterval);
             answerCorrect.style.display = "none";
         }
-
     }, 500);
 }
 
-
-
-
-// answer is Wrong
 function answerIsWrong() {
-    // console.log("nope");
+
     var answerWrong = document.createElement("p");
     answerWrong.textContent = "Wrong";
     answerStat.appendChild(answerWrong);
-    answerWrong.setAttribute("style", "color:orange; font-size:20px; font-weight:bold; margin-top:20px; text-align:center;");
-
+    answerWrong.setAttribute("style", "color:orange; font-size:24px; text-align:center;");
     var answerTime = 1;
     var answerInterval = setInterval(function () {
         answerTime--;
@@ -146,15 +155,11 @@ function answerIsWrong() {
             clearInterval(answerInterval);
             answerWrong.style.display = "none";
         }
-
     }, 500);
-
-
 }
 
-// counter render
+var secondsLeft = 5;
 
-var secondsLeft = 30;
 function setTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
@@ -163,25 +168,20 @@ function setTime() {
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            sendMessage();
+            scoreRender();
+            timeEl.style.display = "none";
         }
 
     }, 1000);
 }
 
-function answerTime() {
+function scoreRender() {
 
-
+    scoreDiv.style.display = "block";
+    quiz.style.display = "none";
+    scoreDiv.innerHTML = "You're done! Let's see how you did...";
 }
 
-function sendMessage() {
-    timeEl.textContent = "you suck";
 
-    var imgEl = document.createElement("img");
-
-    imgEl.setAttribute("src", "images/image_1.jpg");
-    mainEl.appendChild(imgEl);
-
-}
 
 
